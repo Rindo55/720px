99c41b412997883d26b9362fd555113eaa75e5e2
Rindo55 <77073061+Rindo55@users.noreply.github.com>
2023-01-21 23:23:14 +0530

    Update tg_handler.py

--- main/modules/tg_handler.py
+++ main/modules/tg_handler.py
@@ -135,7 +135,7 @@ async def start_uploading(data):
         ghostname = ghostname.replace("(720p)", "")
         
         guessname = f"**{ghostname}**" + "\n" + "✓  `720p x264 Web-DL`" + "\n" + "✓  `English Sub`" + "\n" + f"__({tit})__" + "\n"+ "#Source #WebDL"
-        await client.on_message(filters.regex("480p"))
+        await client.on_message(filters.channel(KAYO_ID) & filters.regex("480p"))
         videox = app.send_document(
 
                 KAYO_ID,
